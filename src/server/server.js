aaa5a40e1c19cf8596fdc95b93578ab8f14d0799
Jakub Fiala <mu201jf@gold.ac.uk>
2016-05-09 12:27:28 +0100

    Pass Test: serverSpec.js

--- src/server/server.js
+++ src/server/server.js
@@ -1,3 +1,24 @@
-module.exports['root'] = function(req, res) {
+function renderList(res, data, numPages, currentLetter, currentPage) {
+	res.send('Hello');
+}
 
+//this can be constant for the purposes of our layout
+const imgRes = '406x228';
+
+module.exports['root'] = function(req, res) {
+	loadData('A', 1, imgRes, function(data, numPages) {
+		renderList(res, data, numPages, 'A', 1);
+	});
+}
+
+module.exports['letter'] = function(req, res) {
+	loadData(req.params.letter, 1, imgRes, function(data, numPages) {
+		renderList(res, data, numPages, req.params.letter, 1);
+	});
+}
+
+module.exports['letter_page'] = function(req, res, next) {
+	loadData(req.params.letter, req.params.page, imgRes, function(data, numPages) {
+		renderList(res, data, numPages, req.params.letter, req.params.page);
+	});
 }
